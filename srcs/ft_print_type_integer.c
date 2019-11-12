6dca25e0a53583d009619e24d4aa0fad132fafcd
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2019-11-12 11:55:06 +0100

    :triangular_flag_on_post: feat: add type %d %i %u and flags management ft_print_type_integer.c

--- srcs/ft_print_type_integer.c
+++ srcs/ft_print_type_integer.c
@@ -6,7 +6,7 @@
 /*   By: jereligi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 11:15:13 by jereligi          #+#    #+#             */
-/*   Updated: 2019/11/11 11:18:10 by jereligi         ###   ########.fr       */
+/*   Updated: 2019/11/12 11:47:33 by jereligi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,4 +40,84 @@ void	ft_putnbr_unsigned(unsigned nbr)
 	}
 	else
 		ft_putchar(nbr + '0');
+}
+
+static void		ft_print_flags_zero(char c, va_list ap, t_data *data)
+{
+	int		i;
+	long	nb;
+	
+	nb = va_arg(ap, long);
+	i = ft_strlen_nb(nb);
+	while (i++ < data->flags_nb)
+		write(1, "0", 1);
+	if (c == 'd' || c == 'i')
+		ft_putnbr(nb);
+	else
+		ft_putnbr_unsigned(nb);
+}
+
+static void		ft_print_flags_minus(char c, va_list ap, t_data *data)
+{
+	int		i;
+	long	nb;
+
+	nb = va_arg(ap, long);
+	i = ft_strlen_nb(nb);
+	if (c == 'd' || c == 'i')
+		ft_putnbr(nb);
+	else
+		ft_putnbr_unsigned(nb);
+	while (i++ < data->flags_nb)
+		write(1, " ", 1);
+}
+
+static void		ft_print_flags_width_precision(char c, va_list ap, t_data *data)
+{
+	int		i;
+	long	nb;
+
+	nb = va_arg(ap, long);
+	i = ft_strlen_nb(nb);
+	if (data->width >= 1)
+		while (i++ < data->width)
+			write(1, " ", 1);
+	else if (data->precision == '*')
+		while (i++ < data->precision_nb)
+			write(1, " ", 1);
+	if (c == 'd' || c == 'i')
+		ft_putnbr(nb);
+	else
+		ft_putnbr_unsigned(nb);
+}
+
+static void		ft_print_flags_point(char c, va_list ap, t_data *data)
+{
+	int		i;
+	long	nb;
+
+	nb = va_arg(ap, long);
+	i = ft_strlen_nb(nb);
+	while (data->precision_nb-- > i)
+		write(1, "0", 1);
+	if (c == 'd' || c == 'i')
+		ft_putnbr(nb);
+	else
+		ft_putnbr_unsigned(nb);
+}
+
+void			ft_print_type_integer(char c, va_list ap, t_data *data)
+{
+	if (data->flags == '0')
+		ft_print_flags_zero(c, ap, data);
+	else if (data->flags == '-')
+		ft_print_flags_minus(c, ap, data);
+	else if (data->width >= 1 || data->precision == '*')
+		ft_print_flags_width_precision(c, ap, data);
+	else if (data->precision == '.')
+		ft_print_flags_point(c, ap, data);
+	else if (c == 'd' || c == 'i')
+		ft_putnbr(va_arg(ap, long));
+	else
+		ft_putnbr_unsigned(va_arg(ap, long));
 }
--- srcs/ft_print_utils.c
+++ srcs/ft_print_utils.c
@@ -6,7 +6,7 @@
 /*   By: jereligi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 11:57:40 by jereligi          #+#    #+#             */
-/*   Updated: 2019/11/11 15:45:26 by jereligi         ###   ########.fr       */
+/*   Updated: 2019/11/12 11:21:17 by jereligi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,31 +26,34 @@ int			ft_strlen_nb(long nb)
 {
 	int	i;
 
-	i = 0;
-	while (nb / 10)
+	i = 1;
+	while (nb > 9)
+	{
+		nb = nb / 10;
 		i++;
+	}
 	return (i);
 }
 
-int			ft_check_flags(const char *s, int i, va_list ap, t_storage *storage)
+int			ft_check_flags(const char *s, int i, va_list ap, t_data *data)
 {
 	if (s[i] == '-')
 	{
-		storage->flags = s[i];
+		data->flags = s[i];
 		i++;
 		while (s[i] >= '0' && s[i] <= '9')
 		{
-			storage->flags_nb = storage->flags_nb * 10 + s[i] - 48;
+			data->flags_nb = data->flags_nb * 10 + s[i] - 48;
 			i++;
 		}
 	}
 	if (s[i] == '0' && s[i - 1] != '-')
 	{
-		storage->flags = '0';
+		data->flags = '0';
 		i++;
 		while (s[i] >= '0' && s[i] <= '9')
 		{
-			storage->flags_nb = storage->flags_nb * 10 + s[i] - 48;
+			data->flags_nb = data->flags_nb * 10 + s[i] - 48;
 			i++;
 		}
 	}
@@ -58,57 +61,57 @@ int			ft_check_flags(const char *s, int i, va_list ap, t_storage *storage)
 	{
 		while (s[i] >= '0' && s[i] <= '9')
 		{
-			storage->width = storage->width * 10 + s[i] - 48;
+			data->width = data->width * 10 + s[i] - 48;
 			i++;
 		}
 	}
 	if (s[i] == '.')
 	{		
-		storage->precision = s[i];
+		data->precision = s[i];
 		i++;
 		while (s[i] >= '0' && s[i] <= '9')
 		{
-			storage->precision_nb = storage->precision_nb * 10 + s[i] - 48;
+			data->precision_nb = data->precision_nb * 10 + s[i] - 48;
 			i++;
 		}
 	}
 	if (s[i] == '*')
 	{
-		storage->precision = s[i];
+		data->precision = s[i];
 		i++;
 		if (ft_is_convers(s[i]))
-			storage->precision_nb = va_arg(ap, int);
+			data->precision_nb = va_arg(ap, int);
 		else
 			while (s[i] >= '0' && s[i] <= '9')
 			{
-				storage->precision_nb = storage->precision_nb * 10 + s[i] - 48;
+				data->precision_nb = data->precision_nb * 10 + s[i] - 48;
 				i++;
 			}
 	}
 	return (i);
 }
 
-void		ft_print_convers(const char *s, int i, va_list ap, t_storage *storage)
+void		ft_print_convers(const char *s, int i, va_list ap, t_data *data)
 {
 	if (s[i] == 'c')
-		ft_print_type_c(ap, storage);
+		ft_print_type_c(ap, data);
 	if (s[i] == 's')
-		ft_print_type_s(ap, storage);
-/*	if (s[i] == 'd' || s[i] == 'u' || s[i] == 'i')
-		ft_print_type_integer(s[i], ap, storage);
-	if (s[i] == 'x' || s[i] == 'X')
-		ft_print_type_convert_hex(s[i], ap, storage);
+		ft_print_type_s(ap, data);
+	if (s[i] == 'd' || s[i] == 'u' || s[i] == 'i')
+		ft_print_type_integer(s[i], ap, data);
+/*	if (s[i] == 'x' || s[i] == 'X')
+		ft_print_type_convert_hex(s[i], ap, data);
 	if (s[i] == 'p')
-		ft_print_type_p(ap, storage);*/
+		ft_print_type_p(ap, data);*/
 }
 
-t_storage	ft_init_struct(t_storage storage)
+t_data	ft_init_struct(t_data data)
 {
-	storage.flags = '~';
-	storage.flags_nb = 0;
-	storage.width = 0;
-	storage.precision = '0';
-	storage.precision_nb = 0;
-	storage.return_printf = 0;
-	return (storage);
+	data.flags = '~';
+	data.flags_nb = 0;
+	data.width = 0;
+	data.precision = '0';
+	data.precision_nb = 0;
+	data.return_printf = 0;
+	return (data);
 }
