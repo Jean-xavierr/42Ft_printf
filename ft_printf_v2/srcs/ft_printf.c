7c738eabc0009f064df3265a42e076207712a8e0
Jean-xavier RELIGIEUX <religieuxjxavier@gmail.com>
2019-11-18 12:03:12 +0100

    :triangular_flag_on_post: feat: add functions for management and check flags

--- ft_printf_v2/srcs/ft_printf.c
+++ ft_printf_v2/srcs/ft_printf.c
@@ -6,41 +6,82 @@
 /*   By: jereligi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 13:44:05 by jereligi          #+#    #+#             */
-/*   Updated: 2019/11/16 16:11:22 by jereligi         ###   ########.fr       */
+/*   Updated: 2019/11/18 11:58:52 by jereligi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
+int		ft_check_is_width_precision(int i, t_data *data)
+{
+	if (data->set[i] >= '0' && data->set[i] <= '9')
+		while (data->set[i] >= '0' && data->set[i] <= '9')
+			data->width = data->width * 10 + (data->set[i++] - '0');
+	else if (data->set[i] == '*')
+	{
+		if (ft_is_convers(data->set[i + 1]) || data->set[i + 1] == '*')
+		{
+			data->width = va_arg(data->ap, int);
+			if (data->width < 0)
+			{
+				data->flags = '-';
+				data->flags_nb = data->width * -1;
+				data->width = 0;
+			}
+			i++;
+		}
+		else
+			while (data->set[i] >= '0' && data->set[i] <= '9')
+				data->width = data->width * 10 + (data->set[i++] - '0');
+	}
+	return (i);
+}
+
+int		ft_check_is_minus_or_zero(int i, t_data *data)
+{
+	if (data->set[i] == '-')
+	{
+		data->flags = '-';
+		i++;
+		while (data->set[i] >= '0' && data->set[i] <= '9')
+			data->flags_nb = data->flags_nb * 10 + (data->set[i++] - '0');
+	}
+	else if (data->set[i] == '0' && data->flags != '-' && data->flags != '0')
+	{
+		data->flags = '0';
+		while (data->set[i] >= '0' && data->set[i] <= '9')
+			data->flags_nb = data->flags_nb * 10 + (data->set[i++] - '0');
+	}
+	return (i);
+}
+
 void	ft_check_flags(t_data *data)
 {
 	int i;
+	int	n;
 
-	i = -1;
-	while (data->set[++i])	
-		if (data->set[i] == '-')
+	i = 0;
+	n = 0;
+	while (data->set[i])
+	{
+		if (i < (n = ft_check_is_minus_or_zero(i, data)))
+			i = n;
+		else if (i < (n = ft_check_is_width_precision(i, data)))
+			i = n;
+		else if (data->set[i++] == '.')
 		{
-			data->flags = '-';
+			if (data->set[i] != '*')
+				while (data->set[i] >= '0' && data->set[i] <= '9')
+					data->precision_nb = data->precision_nb *
+						10 + (data->set[i++] - '0');
+			else if (data->set[i++] == '*')
+				data->precision_nb = va_arg(data->ap, int);
+		}
+		else if (i < (n = ft_check_is_width_precision(i, data)))
+			i = n;
+		else
 			i++;
-			while (data->set[i] >= '0' && data->set[i] <= '9') 
-					data->flags_nb = data->flags_nb * 10 + (data->set[i++] - '0');
-		}
-		else if (data->set[i] == '0' && data->flags != '-' && data->flags != '0')
-			data->flags = '0';
-		else if (data->set[i] >= '0' && data->set[i] <= '9')
-			while (data->set[i] >= '0' && data->set[i] <= '9') 
-				data->width = data->width * 10 + (data->set[i++] - '0');
-		else if (data->set[i] == '.')
-			while (data->set[i] >= '0' && data->set[i] <= '9') 
-				data->precision_nb = data->precision_nb * 10 + (data->set[i++] - '0');
-		else if (data->set[i] == '*' && data->width <= '0')
-		{
-			if (ft_is_convers(data->set[i + 1]))
-				data->width = va_arg(data->ap, int);
-			else
-				while (data->set[i] >= '0' && data->set[i] <= '9') 
-					data->width = data->width * 10 + (data->set[i++] - '0');
-		}
+	}
 }
 
 int		ft_check_string(int i, t_data *data)
@@ -60,7 +101,7 @@ int		ft_check_string(int i, t_data *data)
 	ft_check_flags(data);
 	printf("flags = %c\n", data->flags);
 	printf("flags_nb = %d\n", data->flags_nb);
-	printf("flags_pre = %c\n", data->precision_nb);
+	printf("flags_pre = %d\n", data->precision_nb);
 	printf("flags_width = %d\n\n", data->width);
 /*	while (data->set[n++])
 	{
